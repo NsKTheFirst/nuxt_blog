a0e255c6f65742a5dfc7bc57067dd16f68620330
Nico <nicolas_jouenne@live.fr>
2019-11-05 15:37:22 +0100

    test commit

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,13 +1,19 @@
 <template>
-  $END$
+  <div class="posts-page">
+
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "index"
-    }
+  export default {
+    name: "index"
+  }
 </script>
 
 <style scoped>
-
+  .posts-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
