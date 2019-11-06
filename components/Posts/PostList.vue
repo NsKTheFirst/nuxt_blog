e16d2285c97ba1e02ca267798a7e265a49e1a513
Nico <nicolas_jouenne@live.fr>
2019-11-06 17:15:36 +0100

    add postlist, header, sidenav and sidenavtoggle

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="posts-list">
+    <PostPreview
+      id="1"
+      thumbnail="https://media.dhakatribune.com/uploads/2019/08/bigstock-creating-software-and-website-295627450-1564745123094.jpg"
+      title="Hello there!"
+      previewText="This is my first post!"/>
+    <PostPreview
+      id="2"
+      thumbnail="https://media.dhakatribune.com/uploads/2019/08/bigstock-creating-software-and-website-295627450-1564745123094.jpg"
+      title="Hello there - the second time!"
+      previewText="This is my second post!"/>
+    <PostPreview
+      id="3"
+      thumbnail="https://media.dhakatribune.com/uploads/2019/08/bigstock-creating-software-and-website-295627450-1564745123094.jpg"
+      title="Hi!"
+      previewText="This is my third post!"/>
+  </section>
+</template>
+
+<script>
+  import PostPreview from '@/components/Posts/PostPreview'
+  export default {
+    name: "PostList",
+    components: {
+      PostPreview
+    }
+  }
+</script>
+
+<style scoped>
+  .posts-list {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,16 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview
-        id="1"
-        thumbnail="https://media.dhakatribune.com/uploads/2019/08/bigstock-creating-software-and-website-295627450-1564745123094.jpg"
-        title="Hello there!"
-        previewText="This is my first post!"/>
-      <PostPreview
-        id="2"
-        thumbnail="https://media.dhakatribune.com/uploads/2019/08/bigstock-creating-software-and-website-295627450-1564745123094.jpg"
-        title="Hello there - the second time!"
-        previewText="This is my second post!"/>
-      <PostPreview
-        id="3"
-        thumbnail="https://media.dhakatribune.com/uploads/2019/08/bigstock-creating-software-and-website-295627450-1564745123094.jpg"
-        title="Hi!"
-        previewText="This is my third post!"/>
-    </section>
+    <PostList/>
   </div>
 </template>
 
 <script>
-  import PostPreview from '@/components/Posts/PostPreview'
+  import PostList from '@/components/Posts/PostList'
 
   export default {
     components: {
-      PostPreview
+      PostList
     }
   }
 </script>
@@ -62,14 +46,5 @@
     .intro h1 {
       font-size: 2rem;
     }
-  }
-
-  .featured-posts {
-    display: flex;
-    padding: 20px;
-    box-sizing: border-box;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
   }
 </style>
